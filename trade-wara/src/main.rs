7c949dc7d2f19d4ef6b1a586d10713848e67c811
kalleby santos <kalleby_santos@hotmail.com>
2023-06-22 22:00:09 +0100

    📦TradeWara: Main process order channel workflow.

--- trade-wara/src/main.rs
+++ trade-wara/src/main.rs
@@ -1,3 +1,46 @@
+use std::{
+    sync::mpsc::{channel, TryRecvError},
+    thread,
+    time::Duration,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let orders_in = channel::<u8>();
+    let orders_out = channel::<u8>();
+
+    thread::Builder::new()
+        .name("kafka-listener".into())
+        .spawn(move || loop {
+            // List Kafka here
+            // Send order to orders_in
+            let _ = orders_in.0.send(5).unwrap();
+            println!("Sent 5 from kafka-listener");
+
+            thread::sleep(Duration::from_secs(5))
+        })
+        .unwrap();
+
+    thread::Builder::new()
+        .name("trade-matcher".into())
+        .spawn(move || loop {
+            // Receive from orders_in
+            // Send order to orders_out
+            if let Ok(value) = orders_in.1.try_recv() {
+                println!("Receive {} inside trade-matcher", value);
+
+                let _ = orders_out.0.send(value * 2);
+            }
+        })
+        .unwrap();
+
+    // Order publisher
+    loop {
+        match orders_out.1.try_recv() {
+            Ok(value) => println!("Receive {} in main", value), // Publish processed order Kafka here
+            Err(TryRecvError::Empty) => continue,
+            Err(TryRecvError::Disconnected) => {
+                panic!("The channel has been disconnected, shutting down.");
+            }
+        }
+    }
 }
