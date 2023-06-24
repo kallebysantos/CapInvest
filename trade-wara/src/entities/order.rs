d6fdaea5eaded613915b5d6f3d75b5a8b3545d72
kalleby santos <kalleby_santos@hotmail.com>
2023-06-24 16:34:26 +0100

    📦 TradeWara: Implementing Buy and Sell types for Order

--- trade-wara/src/entities/order.rs
+++ trade-wara/src/entities/order.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{marker::PhantomData, ops::Deref};
 
 use macro_rules_attribute::{derive, derive_alias};
 use trade_wara::ComparableFloat;
@@ -7,6 +7,15 @@ derive_alias! {
     #[derive(Ord!)] = #[derive(PartialEq, Eq, Ord, PartialOrd)];
 }
 
+pub enum Order {
+    Buy(BuyOrder),
+    Sell(SellOrder),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum OrderError {
+    OutRangeShareCount,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum OrderTransition {
@@ -79,6 +88,44 @@ impl OrderInfo<OpenState> {
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
+pub struct BuyOrder<S: OrderState = OpenState>(OrderInfo<S>);
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct SellOrder<S: OrderState = OpenState>(OrderInfo<S>);
+
+impl BuyOrder<OpenState> {
+    pub fn buy(&mut self, share_count: u32) -> Result<Option<BuyOrder<ClosedState>>, OrderError> {
+        if self.0.pending_shares < share_count {
+            return Err(OrderError::OutRangeShareCount);
+        }
+
+        // todo!() increment Investor asset position Here
+        self.0.pending_shares -= share_count;
+
+        match self.0.check_order() {
+            OrderTransition::Open(_) => Ok(None),
+            OrderTransition::Closed(order) => Ok(Some(order.into())),
+        }
+    }
+}
+
+impl SellOrder<OpenState> {
+    pub fn sell(&mut self, share_count: u32) -> Result<Option<SellOrder<ClosedState>>, OrderError> {
+        if self.0.pending_shares < share_count {
+            return Err(OrderError::OutRangeShareCount);
+        }
+
+        // todo!() decrement Investor asset position Here
+        self.0.pending_shares -= share_count;
+
+        match self.0.check_order() {
+            OrderTransition::Open(_) => Ok(None),
+            OrderTransition::Closed(order) => Ok(Some(order.into())),
+        }
+    }
+}
+
 impl<S: OrderState> Ord for OrderInfo<S> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.price.cmp(&other.price)
@@ -90,6 +137,59 @@ impl<S: OrderState> PartialOrd for OrderInfo<S> {
         Some(self.cmp(&other))
     }
 }
+
+impl<S: OrderState> Deref for BuyOrder<S> {
+    type Target = OrderInfo<S>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<S: OrderState> From<OrderInfo<S>> for BuyOrder<S> {
+    fn from(value: OrderInfo<S>) -> Self {
+        Self(value)
+    }
+}
+
+impl<S: OrderState> Ord for BuyOrder<S> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl<S: OrderState> PartialOrd for BuyOrder<S> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
+impl<S: OrderState> Deref for SellOrder<S> {
+    type Target = OrderInfo<S>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<S: OrderState> From<OrderInfo<S>> for SellOrder<S> {
+    fn from(value: OrderInfo<S>) -> Self {
+        Self(value)
+    }
+}
+
+impl<S: OrderState> Ord for SellOrder<S> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
+}
+
+impl<S: OrderState> PartialOrd for SellOrder<S> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::{
@@ -145,4 +245,95 @@ mod tests {
         assert_eq!(OrderTransition::Open(order.copy()), order.check_order());
     }
 
+    #[test]
+    fn heap_sell_orders() {
+        let mut max_heap = BinaryHeap::new();
+        max_heap.push(SellOrder(OrderInfo::new("2".into(), 5.0, 10)));
+        max_heap.push(SellOrder(OrderInfo::new("1".into(), 7.0, 5)));
+        max_heap.push(SellOrder(OrderInfo::new("3".into(), 3.75, 100)));
+
+        assert_eq!(3, max_heap.len());
+        assert_eq!(7.0, *max_heap.pop().unwrap().0.price);
+        assert_eq!(5.0, *max_heap.pop().unwrap().0.price);
+        assert_eq!(3.75, *max_heap.pop().unwrap().0.price);
+
+        let mut min_heap = BinaryHeap::new();
+        min_heap.push(Reverse(OrderInfo::new("2".into(), 5.0, 10)));
+        min_heap.push(Reverse(OrderInfo::new("1".into(), 7.0, 5)));
+        min_heap.push(Reverse(OrderInfo::new("3".into(), 3.75, 100)));
+
+        assert_eq!(3, min_heap.len());
+        assert_eq!(3.75, *min_heap.pop().unwrap().0.price);
+        assert_eq!(5.0, *min_heap.pop().unwrap().0.price);
+        assert_eq!(7.0, *min_heap.pop().unwrap().0.price);
+    }
+
+    #[test]
+    fn check_sell_order_state() {
+        // "Selling more than it owns should return an Err"
+        let mut order = SellOrder(OrderInfo::new("123".into(), 7.0, 5));
+
+        assert_eq!(Err(OrderError::OutRangeShareCount), order.sell(10));
+
+        let mut order = SellOrder(OrderInfo::new("123".into(), 7.0, 5));
+
+        // "Selling less than it owns should be Ok and keep it open"
+        let sell_partial = order.sell(3);
+        assert!(sell_partial.is_ok());
+        assert_eq!(None, sell_partial.unwrap());
+
+        // "Selling all pending shares should be Ok and get Some closed"
+        let sell_remain = order.sell(2);
+        assert!(sell_remain.is_ok());
+        assert_eq!(
+            Some(SellOrder::<ClosedState>(order.0.copy())),
+            sell_remain.unwrap()
+        );
+    }
+
+    #[test]
+    fn heap_buy_orders() {
+        let mut max_heap = BinaryHeap::new();
+        max_heap.push(OrderInfo::new("2".into(), 5.0, 10));
+        max_heap.push(OrderInfo::new("1".into(), 7.0, 5));
+        max_heap.push(OrderInfo::new("3".into(), 3.75, 100));
+
+        assert_eq!(3, max_heap.len());
+        assert_eq!(7.0, *max_heap.pop().unwrap().price);
+        assert_eq!(5.0, *max_heap.pop().unwrap().price);
+        assert_eq!(3.75, *max_heap.pop().unwrap().price);
+
+        let mut min_heap = BinaryHeap::new();
+        min_heap.push(Reverse(OrderInfo::new("2".into(), 5.0, 10)));
+        min_heap.push(Reverse(OrderInfo::new("1".into(), 7.0, 5)));
+        min_heap.push(Reverse(OrderInfo::new("3".into(), 3.75, 100)));
+
+        assert_eq!(3, min_heap.len());
+        assert_eq!(3.75, *min_heap.pop().unwrap().0.price);
+        assert_eq!(5.0, *min_heap.pop().unwrap().0.price);
+        assert_eq!(7.0, *min_heap.pop().unwrap().0.price);
+    }
+
+    #[test]
+    fn check_buy_order_state() {
+        // "Selling more than it owns should return an Err"
+        let mut order = BuyOrder(OrderInfo::new("123".into(), 7.0, 5));
+
+        assert_eq!(Err(OrderError::OutRangeShareCount), order.buy(10));
+
+        let mut order = BuyOrder(OrderInfo::new("123".into(), 7.0, 5));
+
+        // "Selling less than it owns should be Ok and keep it open"
+        let buy_partial = order.buy(3);
+        assert!(buy_partial.is_ok());
+        assert_eq!(None, buy_partial.unwrap());
+
+        // "Selling all pending shares should be Ok and get Some closed"
+        let buy_remain = order.buy(2);
+        assert!(buy_remain.is_ok());
+        assert_eq!(
+            Some(BuyOrder::<ClosedState>(order.0.copy())),
+            buy_remain.unwrap()
+        );
+    }
 }
