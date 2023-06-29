533dc7c130971285413c495d8125c82737da6bd3
kalleby santos <kalleby_santos@hotmail.com>
2023-06-29 09:44:53 +0100

    📦 TradeWara: Impl. Investor's asset manipulation functions

--- trade-wara/src/entities/investor.rs
+++ trade-wara/src/entities/investor.rs
@@ -19,5 +19,73 @@ impl Investor {
         }
     }
 
+    pub fn increment_asset(&mut self, asset_id: String, quantity: u32) {
+        self.assets
+            .entry(asset_id)
+            .and_modify(|current| *current += quantity)
+            .or_insert(quantity);
+    }
+
+    pub fn decrement_asset(
+        &mut self,
+        asset_id: String,
+        quantity: u32,
+    ) -> Result<(), String> {
+        let Some(asset) = self.assets.get_mut(&asset_id)  else {
+            return  Err("Asset not found".into());
+        };
+
+        if *asset < quantity {
+            return Err("Out range quantity".into());
+        }
+
+        *asset = quantity;
+
+        Ok(())
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn increment_investor_assets() {
+        let mut investor = Investor::new(
+            "123".into(),
+            "Joe".into(),
+            vec![("HGLG11".into(), 10)],
+        );
+
+        investor.increment_asset("MXRF11".into(), 20);
+        investor.increment_asset("HGLG11".into(), 5);
+
+        assert_eq!(investor.assets["MXRF11"], 20);
+        assert_eq!(investor.assets["HGLG11"], 15);
+        assert_eq!(investor.assets.len(), 2);
+    }
+
+    #[test]
+    fn decrement_investor_assets() {
+        let mut investor = Investor::new(
+            "123".into(),
+            "Joe".into(),
+            vec![("HGLG11".into(), 10)],
+        );
+
+        assert_eq!(
+            Err("Asset not found".into()),
+            investor.decrement_asset("MXRF11".into(), 20)
+        );
+
+        assert_eq!(Ok(()), investor.decrement_asset("HGLG11".into(), 5));
+
+        assert_eq!(
+            Err("Out range quantity".into()),
+            investor.decrement_asset("HGLG11".into(), 6)
+        );
+
+        assert_eq!(investor.assets["HGLG11"], 5);
+        assert_eq!(investor.assets.len(), 1);
+    }
+}
