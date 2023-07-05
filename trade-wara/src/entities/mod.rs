f65f77320fe17e26755cc399f74557e50c156962
kalleby santos <kalleby_santos@hotmail.com>
2023-07-05 09:06:24 +0100

    📦 TradeWara: Creating Transaction struct

--- trade-wara/src/entities/mod.rs
+++ trade-wara/src/entities/mod.rs
@@ -1,3 +1,4 @@
 pub mod asset;
 pub mod investor;
 pub mod order;
+pub mod transaction;
---
+++ trade-wara/src/entities/transaction.rs
@@ -0,0 +1,31 @@
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+
+use super::order::{Buy, OrderTransition, Sell};
+
+struct Transaction {
+    id: String,
+    buying_order: OrderTransition<Buy>,
+    selling_order: OrderTransition<Sell>,
+    traded_shares: u32,
+    total: f32,
+    traded_at: DateTime<Utc>,
+}
+
+impl Transaction {
+    pub fn new(
+        buying_order: OrderTransition<Buy>,
+        selling_order: OrderTransition<Sell>,
+        shares: u32,
+        price: f32,
+    ) -> Transaction {
+        Transaction {
+            id: Uuid::new_v4().to_string(),
+            buying_order,
+            selling_order,
+            traded_shares: shares,
+            total: shares as f32 * price,
+            traded_at: Utc::now(),
+        }
+    }
+}
